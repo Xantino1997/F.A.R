8f04efcf2dfe839cd88f7993856a6ff9740b9549
Cirene1997 <gjoaquinreynoso@gmail.com>
2023-09-21 16:52:38 -0300

    add the icon

--- src/pages/Contactanos.js
+++ src/pages/Contactanos.js
@@ -1,24 +1,27 @@
 import React from "react";
 import "./stylesPages/Contactanos.css";
 import genteTrabaja from "../assets/motorfondo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faPhone, faMapMarker } from "@fortawesome/free-solid-svg-icons";
+
 const Contacto = () => {
   return (
     <div className="contacto-container">
       <div className="overlay">
-      <img src={genteTrabaja} alt="Fondo" className="video-bg" />
+        <img src={genteTrabaja} alt="Fondo" className="video-bg" />
       </div>
       <div className="contacto-info">
         <div className="gmail-container">
-          <h3>Gmail:</h3>
-          <p> correo@gmail.com</p>
+          <FontAwesomeIcon icon={faEnvelope} className="icon" />
+          <p>farenergetica@gmail.com</p>
         </div>
         <div className="direccion-container">
-          <h3>Dirección:</h3>
-          <p>123 Calle Principal</p>
+          <FontAwesomeIcon  icon={faMapMarker} className="icon" /> {/* Icono de Ubicación */}
+          <p>Galvez Sana Fe</p> {/* Reemplaza "Tu Ubicación Aquí" con la ubicación deseada */}
         </div>
         <div className="telefono-container">
-          <h3>Teléfono:</h3>
-          <p> (123) 456-7890</p>
+          <FontAwesomeIcon icon={faPhone} className="icon" />
+          <p>(341) 5327122</p>
         </div>
       </div>
     </div>
