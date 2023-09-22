a9e9bd71fe4b6ca493c6cb2c396587c3c3edbe55
Cirene1997 <gjoaquinreynoso@gmail.com>
2023-09-22 13:13:52 -0300

    add the icon

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./styles/Footer.css";
 import { Link } from "react-router-dom";
-import facebook from "./assets/facebook.png";
-import instagram from "./assets/instagram.png";
-import whatsapp from "./assets/whatsapp.png";
-import youtube from "./assets/youtube.png";
-import telefono from "./assets/telefono.png";
-import mail from "./assets/mail.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faInstagram,
+  faYoutube,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
+
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 
 function Footer() {
   const currentYear = new Date().getFullYear();
@@ -14,41 +17,12 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="container-column-footer">
-        {/* <div className="column">
-                    <h3 className="column-title">Partners</h3>
-                    <ul className='partner-list'>
-                        <li>
-                            <Link className='partner-list-link' to="https://www.argentina.gob.ar/" target="_blank">
-                              
-                                <span className="social-text">Gobierno de la Nacion</span>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link className='partner-list-link' to="https://www.santafe.gov.ar/" target="_blank">
-                               
-                                <span className="social-text">Gobierno de Santa Fe</span>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link className='partner-list-link' to="https://www.facebook.com/melincuecomuna" target="_blank">
-                                <img className='escudo' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNjeDj1qwNLWr9RcULQBIFG4L5NCmGMzFuIWKsEXSMfT1Qvj_7Lu-oqL76L9UQjy3Ydqs&usqp=CAU' alt='Comuna de Melincue' />
-                                <span className="social-text">Comuna de Melincue</span>
-                            </Link>
-                        </li>
-                    </ul>
-                </div> */}
-
         <div className="column">
           <h3 className="column-title">Contacto</h3>
           <ul className="contacto-list">
             <li>
-              <img
-                className="contactos"
-                src={mail}
-                alt="Correo"
-              />
+              <FontAwesomeIcon icon={faEnvelope} className="contactos" />
               <a
-                target="_blank"
                 className="contacto-list-link"
                 href="mailto:arielaraujo2023@gmail.com"
               >
@@ -56,18 +30,14 @@ function Footer() {
               </a>
             </li>
             <li>
-              <img className="contactos" src={telefono} alt="Telefono" />
-              <a
-                target="_blank"
-                className="contacto-list-link"
-                href="tel:03465499170"
-              >
+              <FontAwesomeIcon icon={faPhone} className="contactos" />
+              <a className="contacto-list-link" href="tel:03465499170">
                 03465-499 170
               </a>
             </li>
             <li>
-              <img className="contactos" src={whatsapp} alt="Whatsapp" />
-              <a target="_blank" className="contacto-list-link" href="#">
+              <FontAwesomeIcon icon={faWhatsapp} className="contactos" />
+              <a className="contacto-list-link" href="#">
                 Whatsapp
               </a>
             </li>
@@ -77,38 +47,37 @@ function Footer() {
           <h3 className="column-title">Redes Sociales</h3>
           <ul className="redes-list">
             <li>
-              <Link
+              <a
                 className="redes-list-link"
-                to="https://www.facebook.com/"
+                href="https://www.facebook.com/"
                 target="_blank"
+                rel="noopener noreferrer"
               >
-                <img
-                  className="social-facebook"
-                  src={facebook}
-                  alt="Facebook"
-                />
+                <FontAwesomeIcon icon={faFacebook} className="social-icon" />
                 <span className="social-text">Facebook</span>
-              </Link>
+              </a>
             </li>
             <li>
-              <Link
+              <a
                 className="redes-list-link"
-                to="https://www.instagram.com/"
+                href="https://www.instagram.com/"
                 target="_blank"
+                rel="noopener noreferrer"
               >
-                <img
-                  className="social-instagram"
-                  src={instagram}
-                  alt="Instagram"
-                />
+                <FontAwesomeIcon icon={faInstagram} className="social-icon" />
                 <span className="social-text">Instagram</span>
-              </Link>
+              </a>
             </li>
             <li>
-              <Link className="redes-list-link" to="#" target="_blank">
-                <img className="social-youtube" src={youtube} alt="Youtube" />
+              <a
+                className="redes-list-link"
+                href="https://www.youtube.com/" // Reemplaza con la URL de tu canal de YouTube
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon icon={faYoutube} className="social-icon" />
                 <span className="social-text">Youtube</span>
-              </Link>
+              </a>
             </li>
           </ul>
         </div>
